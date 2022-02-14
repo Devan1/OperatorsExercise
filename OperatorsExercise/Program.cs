c302a7339c05f504a76f860438f1e9d69935477c
Devan <deshlock010101@hotmai.com>
2022-02-14 11:31:08 -0600

    finished exercises 1 and 2.

--- OperatorsExercise/Program.cs
+++ OperatorsExercise/Program.cs
@@ -6,7 +6,25 @@ namespace OperatorsExercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            
+
+            Console.WriteLine($"{a} divided by {b} is {quotient} and the remainder is {remainder}");
+            Console.Write("What is the radius of your circle?: ");
+            double r = double.Parse(Console.ReadLine());
+            Console.Write($"The area of a circle with the radius of {r} is {AreaOfCircle(r)}");
+            Console.WriteLine();
+                
+            
         }
+        
+        public static double AreaOfCircle(double r)
+        {
+            return (3.14 * (r*r));
+        }
+        
     }
 }
